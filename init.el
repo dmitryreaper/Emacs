2a9c05d9d7c5f06c73838d91e50b70b43d23fc36
Dmitry Dubnovitskiy <78143204+dmitryreaper@users.noreply.github.com>
2024-06-24 13:42:23 +0300

    Add files via upload

--- init.el
+++ init.el
@@ -1,201 +1,65 @@
-(tab-bar-mode 1)
-(setq inhibit-startup-message t) ;Disable open first message startup
-(scroll-bar-mode -1)             ;Disable scroll bar
-(tool-bar-mode -1)               ;Disable toolbar
-(tooltip-mode -1)                ;Disable tooltip
-(menu-bar-mode -1)
-(set-fringe-mode 0)              ;Border frame
-
-;TABBBBBBBB
-(setq-default tab-width          4)
-(setq-default c-basic-offset     4)
-(setq-default standart-indent    4)
-(setq-default tab-always-indent t)
-(defun my-insert-tab-char ()
-
-  "insert a tab char. (ASCII 9, \t)"
-  (interactive)
-  (insert "\t"))
-(global-set-key (kbd "TAB") 'my-insert-tab-char)
-
-;transparency
-(set-frame-parameter nil 'alpha-background 86)
-
-
-(global-display-line-numbers-mode) ;Line number mode
-
-;smooth scrolling
-(setq redisplay-dont-pause t
-	  scroll-margin 5
-	  scroll-step 1
-	  scroll-conservatively 10000
-	  scroll-preserve-screen-position 1)
-
-
-;disable line numbers for some modes
-(dolist  (mode '(org-mode-hook
-		 term-mode-hook
-		 shell-mode-hook
-		 eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;disable backup files,autosaving
-(setq make-backup-files   nil)
-(setq auto-safe-list-file-name    nil)
-(setq auto-safe-default    nil)
-
-;Disable beep signal
-(setq visible-bell t)
-
-;Set default theme
-;doom-ir-black, doom-homage-black,
-(load-theme 'doom-ir-black t)
-
-
-;global exit
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Initialize package sources
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-(unless package-archive-contents
-(package-refresh-contents))
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
 
-(use-package ivy
-  :diminish
-  :bind(("C-s" . swiper)
-	:map ivy-minibuffer-map
-	("TAB" . ivy-alt-done)
-	("C-l" . ivy-alt-done)
-	("C-j" . ivy-next-line)
-	("C-k" . ivy-previous-line)
-	:map ivy-switch-buffer-map
-	("C-k" . ivy-previous-line)
-	("C-l" . ivy-done)
-	("C-d" . ivy-switch-buffer-kill)
-	:map ivy-reverse-i-search-map
-	("C-k" . ivy-previous-line)
-	("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
-
-;all icons
-(use-package all-the-icons
-  :if (display-graphic-p))
-;icons for directory
-(use-package all-the-icons-dired
-  :if (display-graphic-p)
-  :hook (dired-mode . all-the-icons-dired-mode))
-
-;airline
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
-
-(use-package counsel
-  :bind(("M-x" . counsel-M-x)
-	("C-x b" . counsel-ibuffer)
-	("C-x C-f" . counsel-find-file)
-	:map minibuffer-local-map
-	("C-r" . 'counsel-minibuffer-history)))
-
-(use-package doom-themes)
-
-;(use-package general
-;	:config
-;	(general-create-definer rune/leader-keys
-;  	:keymaps '(normal insert visual emacs)
-;  	:prefix "SPC"
-;  	:global-prefix "C-SPC")
-
-;	(rune/leader-keys
-;		"t" '(:ignore t :which-key "toggles")
-;		"tt" '(counsel-load-theme :which-key "choose theme")))
-
-;EVIL MODE
-
-(defun rune/evil-hook ()
-  (dolist (mode '(custom-mode
-				  eshell-mode
-				  git-rebase-mode
-				  erc-mode
-				  circe-server-mode
-				  circe-chat-mode
-				  circe-query-mode
-				  sauron-mode
-				  term-modeE))
-	(add-to-list 'evil-emacs-state-modes mode)))
-
+(set-face-attribute 'default nil :font "Hack Nerd Font-11")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;evil mode
 (use-package evil
+  :demand t
   :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
+
+  ;; see `undo-fu' package.
+  (setq evil-undo-system 'undo-fu)
+  ;; For some reasons evils own search isn't default.
+  (setq evil-search-module 'evil-search)
+
   :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "M-h") 'evil-normal-state) ;fast exit in normal mode
-  (define-key evil-insert-state-map (kbd "M-l") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "M-k") 'evil-normal-state)
+  ;; Initialize.
+  (evil-mode)
+  (define-key evil-insert-state-map (kbd "M-h") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "M-j") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "M-k") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "M-l") 'evil-normal-state)
+  (setq evil-ex-search-case 'sensitive));;;;;;;;;;;;
 
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'message-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-
-(use-package evil-collection
-  :after evil
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package ivy
+  :demand t
   :config
-  (evil-collection-init))
+  (ivy-mode)
 
-;managment project
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  (when (file-directory-p "~/Src/")
-	(setq projectile-project-search-path '("~/Src/")))
-  (setq projectile-switch-project-action #'projectile-dired))
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
+  ;; Always show half the window height. Why?
+  ;; .. useful when searching through large lists of content.
+  (setq ivy-height-alist `((t . ,(lambda (_caller) (/ (frame-height) 2)))))
 
-;lsp configuration
+  ;; VIM style keys in ivy (holding Control).
+  (define-key ivy-minibuffer-map (kbd "C-j") 'next-line)
+  (define-key ivy-minibuffer-map (kbd "C-k") 'previous-line)
+
+  (define-key ivy-minibuffer-map (kbd "C-h") 'minibuffer-keyboard-quit)
+  (define-key ivy-minibuffer-map (kbd "C-l") 'ivy-done)
+
+  ;; open and next
+  (define-key ivy-minibuffer-map (kbd "C-M-j") 'ivy-next-line-and-call)
+  (define-key ivy-minibuffer-map (kbd "C-M-k") 'ivy-previous-line-and-call)
+
+  (define-key ivy-minibuffer-map (kbd "<C-return>") 'ivy-done)
+
+  ;; so we can switch away
+  (define-key ivy-minibuffer-map (kbd "C-w") 'evil-window-map))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (use-package lsp-mode
   :ensure t
   :hook (c++-mode . lsp)
- 	    (python-mode . lsp)
-  		(c-mode . lsp)
-		(nasm-mode . lsp))
+        (c-mode . lsp))
 
 (use-package lsp-ui
   :ensure t
@@ -211,42 +75,234 @@
 	      ("backtab" . company-select-previous))
   :custom
   (company-minimum-prefix-lenght 1)
-  (company-idle-delay 0.01))
+  (company-idle-delay 0.01)
+  :config
+  (define-key company-active-map (kbd "C-j") 'company-select-next-or-abort)
+  (define-key company-active-map (kbd "C-k") 'company-select-previous-or-abort)
+  (define-key company-active-map (kbd "C-l") 'company-complete-selection)
+  (define-key company-active-map (kbd "C-h") 'company-abort)
+  (define-key company-active-map (kbd "<C-return>") 'company-complete-selection)
+
+  (define-key company-search-map (kbd "C-j") 'company-select-next)
+  (define-key company-search-map (kbd "C-k") 'company-select-previous))
 
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
 
 
-;GIT
-(use-package magit
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+;;evil mode config
+;; remove all keybindings from insert-state keymap
 
-;orgmode
-(defun dw/org-mode-setup ()
-	(org-indent-mode)
-	(variable-pitch-mode 1)
-	(auto-fill-mode 1)
-	(visual-line-mode 1)
-	(setq evil-auto-indent nil))
 
-(use-package org
-  :hook (org-mode . dw/org-mode-setup)
+(use-package consult
+  :ensure t
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
   :config
-  (setq org-ellipsis " ^"
-		org-hide-emphasis-markers t))
-(use-package evil-tabs)
-(global-evil-tabs-mode t)
 
-;; АВТОЗАКРЫТИЕ!!!!!!!!!!!!!!!
-(electric-pair-mode    1)
-(electric-indent-mode -1)
-(setq electric-pair-pairs '(
-                                (?\" . ?\")
-                                (?\{ . ?\})
-                                (?\< . ?\>)
-                            ) )
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 3. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  ;;;; 4. projectile.el (projectile-project-root)
+  ;; (autoload 'projectile-project-root "projectile")
+  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 5. No project support
+  ;; (setq consult-project-function nil)
+)
+
+;; Enable vertico
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :ensure t
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :ensure t
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+;; optionally use the `orderless' completion style.
+(use-package orderless
+  :ensure t
+ :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-blinks 100000000000)
+ '(custom-enabled-themes '(doom-1337))
+ '(custom-safe-themes
+   '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "f4d1b183465f2d29b7a2e9dbe87ccc20598e79738e5d29fc52ec8fb8c576fcfd" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
+ '(line-number-mode t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   '(doom-modeline god-mode ivy flycheck company vertico use-package consult lsp-ui lsp-mode evil doom-themes))
+ '(scroll-bar-mode nil)
+ '(tab-bar-mode t)
+ '(tool-bar-mode nil))
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -254,10 +310,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(evil-tabs which-key wfnames request rainbow-delimiters popup nasm-mode magit lsp-ui lsp-pyright ivy-rich hydra general flycheck evil-collection doom-themes doom-modeline counsel-projectile company async all-the-icons-dired)))
