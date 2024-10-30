55c30bcfd791333b0288524a262c39b674f2776b
Dmitry Dubnovitskiy <78143204+dmitryreaper@users.noreply.github.com>
2024-10-30 20:22:00 +0300

    Update windows_init.el

--- windows_init.el
+++ windows_init.el
@@ -1,67 +1,174 @@
 (require 'package)
-  (add-to-list 'package-archives
-               '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+						 ("org" . "https://orgmode.org/elpa/")
+						 ("elpa" . "https://elpa.gnu.org/packages/")))  
 (package-initialize)
 
-;;maximize screen
-(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;;default directory
 (setq default-directory "~/") 
 
-;;inhibit splash screen
-(setq inhibit-splash-screen t)
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;;change the font
-(set-face-attribute 'default nil :font "Hack Nerd Font-12")
+;;BASIC UI CONFIGURATION
+(setq inhibit-startup-message t)
 
-;;disable menu bar
-(menu-bar-mode -1)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
 
-;;disable tool bar
-(tool-bar-mode -1)
-
-;;cursor-blinks forever
 (setq blink-cursor-blinks 0)
 
-;;disable scrol bar
-(scroll-bar-mode -1)
+;; tabs
+(setq-default tab-width          4)
+(setq-default c-basic-offset     4)
+(setq-default standart-indent    4)
 
+(menu-bar-mode -1)            ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Set frame transparency
+(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+
+;;colors CURSOR
+(set-frame-parameter nil 'cursor-color "#ff0000")
+(add-to-list 'default-frame-alist '(cursor-color . "#ff0000"))  ;; Disable line numbers for some modes
+
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;;FONT
+(set-face-attribute 'default nil :font "Hack Nerd Font-11")
+
+;;KEYBINDING 
 ;;set "gnu" style for c
 (setq c-deafault-style "linux"
       c-basic-offset 4)
 
-;;doom themes
-(use-package doom-themes
-  :init (load-theme 'doom-ir-black t))
-
 ;;auto pairnn
 (electric-pair-mode 1)
 
-;;transparent
-(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
-(add-to-list 'default-frame-alist '(alpha . (85 .50)))
-
-;;cursor color
-(set-frame-parameter nil 'cursor-color "#ff0000")
-(add-to-list 'default-frame-alist '(cursor-color . "#ff0000"))
-
-;;evil-mode for vim layout
-(use-package evil
-  :ensure t
-  :init (evil-mode 1)
+;;Ivy and Counsel
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
   :config
-  (define-key evil-insert-state-map (kbd "M-h") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "M-j") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "M-k") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "M-l") 'evil-normal-state))
+  (ivy-mode 1))
 
+(use-package ivy-rich
+  :after ivy
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("C-M-j" . 'counsel-switch-buffer)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (counsel-mode 1))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;LSP SERVERS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;c/C++ MODE
+(use-package lsp-mode
+  :ensure t
+  :hook
+  (c++-mode . lsp)
+  (java-mode . lsp)
+  (c-mode . lsp))
+
+;;python
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp))))  ; or lsp-deferred
+;;UI LSP WIH CURSOR
+(use-package lsp-ui
+  :ensure t
+  :after lsp-mode
+  :custom
+  (lsp-ui-doc-show-with-cursor t))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+
+;;IMPROVED CANDIDATE SORTING WITH PRESCIENT.EL 
+(use-package ivy-prescient
+  :after counsel
+  :custom
+  (ivy-prescient-enable-filtering nil)
+  :config
+  ;; Uncomment the following line to have sorting remembered across sessions!
+										;(prescient-persist-mode 1)
+  (ivy-prescient-mode 1))
+
+;;HELPFUL HELP COMMANDS
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; git
+
+(use-package magit
+  :commands magit-status
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; NOTE: Make sure to configure a GitHub token before using this package!
+;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
+;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
+(use-package forge
+  :after magit)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes use-package evil)))
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd" default))
+ '(package-selected-packages
+   '(lsp-java gruber-darker-theme forge org-bullets helpful ivy-prescient flycheck lsp-ui lsp-pyright lsp-mode counsel ivy-rich ivy doom-modeline all-the-icons doom-themes evil-collection use-package spinner nerd-icons markdown-mode magit lv ht f evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
